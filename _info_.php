9451cb05cb891060a24ba7ecbde705de50d9ec06
xtr4nge <xtr4nge@gmail.com>
2014-12-07 22:34:36 +0000

    minor fix ($log_path)

--- _info_.php
+++ _info_.php
@@ -2,7 +2,7 @@
 $mod_name="mana";
 $mod_version="1.0";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
-$mod_logs="/usr/share/fruitywifi/logs/$mod_name.log"; 
+$mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
 $mod_panel="show";
 $mod_type="service";
