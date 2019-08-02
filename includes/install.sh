ce021ea0e53269b263044ca7d66929d567fa2707
xtr4nge <xtr4nge@gmail.com>
2019-08-02 14:35:37 +0100

    minor update

--- includes/install.sh
+++ includes/install.sh
@@ -8,9 +8,9 @@ apt-get -y install libnl1 libnl-dev libssl-dev
 
 apt-get -y install libnl-dev libssl-dev #isc-dhcp-server tinyproxy  apache2 macchanger python-dnspython python-pcapy dsniff stunnel4
 
-apt-get -y install gcc-4.7
-apt-get -y install g++-4.7
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+#apt-get -y install gcc-4.7
+#apt-get -y install g++-4.7
+#update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
 
 echo "installing Hostapd/Mana..."
 # INSTALL HOSTAPD-MANA
@@ -18,25 +18,25 @@ wget https://github.com/xtr4nge/hostapd-mana/archive/master.zip -O hostapd-mana.
 
 unzip hostapd-mana.zip
 
-cmd=`lsb_release -c |grep -iEe "jessie|kali|sana"`
-if [[ ! -z $cmd ]]
-then
-    echo "--------------------------------"
-    echo "ADDING: CONFIG_LIBNL32=y (Debian Jessie|Kali patch)"
-    echo "--------------------------------"
+#cmd=`lsb_release -c |grep -iEe "jessie|kali|sana"`
+#if [[ ! -z $cmd ]]
+#then
+#    echo "--------------------------------"
+#    echo "ADDING: CONFIG_LIBNL32=y (Debian Jessie|Kali patch)"
+#    echo "--------------------------------"
     
-    apt-get -y install libnl-3-dev libnl-genl-3-dev
+#    apt-get -y install libnl-3-dev libnl-genl-3-dev
     
-    EXEC="s,^#CONFIG_LIBNL32=y,CONFIG_LIBNL32=y,g"
-    sed -i $EXEC hostapd-mana-master/hostapd/.config
+#    EXEC="s,^#CONFIG_LIBNL32=y,CONFIG_LIBNL32=y,g"
+#    sed -i $EXEC hostapd-mana-master/hostapd/.config
 	
-	EXEC="s,^#CONFIG_DEBUG_FILE=y,CONFIG_DEBUG_FILE=y,g"
-    sed -i $EXEC hostapd-mana-master/hostapd/.config
+#    EXEC="s,^#CONFIG_DEBUG_FILE=y,CONFIG_DEBUG_FILE=y,g"
+#    sed -i $EXEC hostapd-mana-master/hostapd/.config
     
-    echo "[setup completed]"
-    echo
+#    echo "[setup completed]"
+#    echo
 
-fi
+#fi
 
 cd hostapd-mana-master/hostapd
 make
